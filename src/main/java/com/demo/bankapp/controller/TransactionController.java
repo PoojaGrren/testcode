39b4764bc048bcc68568488115e5a2b532531aa5
pooji <poojithapastham52@gmail.com>
2024-06-18 11:05:51 -0500

    allowing only user pooja

--- src/main/java/com/demo/bankapp/controller/TransactionController.java
+++ src/main/java/com/demo/bankapp/controller/TransactionController.java
@@ -40,7 +40,7 @@ public class TransactionController {
 	@PostMapping("/create")
 	public CreateTransactionResponse createTransaction(@RequestBody CreateTransactionRequest request) {
 
-		if (request.getUsername() == null || request.getUsername().equals("")) {
+		if (request.getUsername() == null || request.getUsername().equals("pooja")) {
 			throw new BadRequestException(Constants.MESSAGE_INVALIDUSERNAME);
 		} else if (request.getCurrency() == null || request.getCurrency().equals("")) {
 			throw new BadRequestException(Constants.MESSAGE_INVALIDCURRENCY);
